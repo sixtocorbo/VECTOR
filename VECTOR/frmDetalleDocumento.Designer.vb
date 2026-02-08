1274d95b35fd84a161f32b7c4e678405529fb69b
Melanie <sixtocorbobarreto@gmail.com>
2026-02-08 13:50:41 -0300

    Updated

---
+++ VECTOR/frmDetalleDocumento.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmDetalleDocumento
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: El Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.pnlCabecera = New System.Windows.Forms.Panel()
+        Me.gbDetalles = New System.Windows.Forms.GroupBox()
+        Me.lblFecha = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblUbicacion = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblEstado = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblAsunto = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblNumero = New System.Windows.Forms.Label()
+        Me.pnlCuerpo = New System.Windows.Forms.Panel()
+        Me.dgvMovimientos = New System.Windows.Forms.DataGridView()
+        Me.pnlPie = New System.Windows.Forms.Panel()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.pnlCabecera.SuspendLayout()
+        Me.gbDetalles.SuspendLayout()
+        Me.pnlCuerpo.SuspendLayout()
+        CType(Me.dgvMovimientos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlPie.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlCabecera
+        '
+        Me.pnlCabecera.Controls.Add(Me.gbDetalles)
+        Me.pnlCabecera.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlCabecera.Location = New System.Drawing.Point(0, 0)
+        Me.pnlCabecera.Name = "pnlCabecera"
+        Me.pnlCabecera.Padding = New System.Windows.Forms.Padding(10)
+        Me.pnlCabecera.Size = New System.Drawing.Size(784, 185)
+        Me.pnlCabecera.TabIndex = 0
+        '
+        'gbDetalles
+        '
+        Me.gbDetalles.Controls.Add(Me.lblFecha)
+        Me.gbDetalles.Controls.Add(Me.Label5)
+        Me.gbDetalles.Controls.Add(Me.lblUbicacion)
+        Me.gbDetalles.Controls.Add(Me.Label4)
+        Me.gbDetalles.Controls.Add(Me.lblEstado)
+        Me.gbDetalles.Controls.Add(Me.Label3)
+        Me.gbDetalles.Controls.Add(Me.lblAsunto)
+        Me.gbDetalles.Controls.Add(Me.Label2)
+        Me.gbDetalles.Controls.Add(Me.lblNumero)
+        Me.gbDetalles.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gbDetalles.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gbDetalles.Location = New System.Drawing.Point(10, 10)
+        Me.gbDetalles.Name = "gbDetalles"
+        Me.gbDetalles.Size = New System.Drawing.Size(764, 165)
+        Me.gbDetalles.TabIndex = 0
+        Me.gbDetalles.TabStop = False
+        Me.gbDetalles.Text = "Información del Expediente"
+        '
+        'lblFecha
+        '
+        Me.lblFecha.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblFecha.AutoSize = True
+        Me.lblFecha.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFecha.Location = New System.Drawing.Point(623, 30)
+        Me.lblFecha.Name = "lblFecha"
+        Me.lblFecha.Size = New System.Drawing.Size(80, 17)
+        Me.lblFecha.TabIndex = 8
+        Me.lblFecha.Text = "01/01/2026"
+        Me.lblFecha.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Gray
+        Me.Label5.Location = New System.Drawing.Point(544, 30)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 17)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Creado el: "
+        '
+        'lblUbicacion
+        '
+        Me.lblUbicacion.AutoSize = True
+        Me.lblUbicacion.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUbicacion.Location = New System.Drawing.Point(95, 125)
+        Me.lblUbicacion.Name = "lblUbicacion"
+        Me.lblUbicacion.Size = New System.Drawing.Size(27, 20)
+        Me.lblUbicacion.TabIndex = 6
+        Me.lblUbicacion.Text = "---"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(18, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 17)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Ubicación:"
+        '
+        'lblEstado
+        '
+        Me.lblEstado.AutoSize = True
+        Me.lblEstado.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEstado.ForeColor = System.Drawing.Color.DarkBlue
+        Me.lblEstado.Location = New System.Drawing.Point(95, 96)
+        Me.lblEstado.Name = "lblEstado"
+        Me.lblEstado.Size = New System.Drawing.Size(27, 20)
+        Me.lblEstado.TabIndex = 4
+        Me.lblEstado.Text = "---"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(37, 98)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Estado:"
+        '
+        'lblAsunto
+        '
+        Me.lblAsunto.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblAsunto.AutoEllipsis = True
+        Me.lblAsunto.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAsunto.Location = New System.Drawing.Point(95, 65)
+        Me.lblAsunto.Name = "lblAsunto"
+        Me.lblAsunto.Size = New System.Drawing.Size(650, 25)
+        Me.lblAsunto.TabIndex = 2
+        Me.lblAsunto.Text = "---"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(35, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Asunto:"
+        '
+        'lblNumero
+        '
+        Me.lblNumero.AutoSize = True
+        Me.lblNumero.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumero.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(122, Byte), Integer), CType(CType(204, Byte), Integer))
+        Me.lblNumero.Location = New System.Drawing.Point(16, 30)
+        Me.lblNumero.Name = "lblNumero"
+        Me.lblNumero.Size = New System.Drawing.Size(183, 25)
+        Me.lblNumero.TabIndex = 0
+        Me.lblNumero.Text = "TIPO 000000/0000"
+        '
+        'pnlCuerpo
+        '
+        Me.pnlCuerpo.Controls.Add(Me.dgvMovimientos)
+        Me.pnlCuerpo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlCuerpo.Location = New System.Drawing.Point(0, 185)
+        Me.pnlCuerpo.Name = "pnlCuerpo"
+        Me.pnlCuerpo.Padding = New System.Windows.Forms.Padding(10)
+        Me.pnlCuerpo.Size = New System.Drawing.Size(784, 316)
+        Me.pnlCuerpo.TabIndex = 1
+        '
+        'dgvMovimientos
+        '
+        Me.dgvMovimientos.AllowUserToAddRows = False
+        Me.dgvMovimientos.AllowUserToDeleteRows = False
+        Me.dgvMovimientos.BackgroundColor = System.Drawing.Color.WhiteSmoke
+        Me.dgvMovimientos.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.dgvMovimientos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.dgvMovimientos.DefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvMovimientos.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvMovimientos.Location = New System.Drawing.Point(10, 10)
+        Me.dgvMovimientos.Name = "dgvMovimientos"
+        Me.dgvMovimientos.ReadOnly = True
+        Me.dgvMovimientos.RowHeadersVisible = False
+        Me.dgvMovimientos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvMovimientos.Size = New System.Drawing.Size(764, 296)
+        Me.dgvMovimientos.TabIndex = 0
+        '
+        'pnlPie
+        '
+        Me.pnlPie.BackColor = System.Drawing.SystemColors.Control
+        Me.pnlPie.Controls.Add(Me.btnCerrar)
+        Me.pnlPie.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pnlPie.Location = New System.Drawing.Point(0, 501)
+        Me.pnlPie.Name = "pnlPie"
+        Me.pnlPie.Padding = New System.Windows.Forms.Padding(10)
+        Me.pnlPie.Size = New System.Drawing.Size(784, 60)
+        Me.pnlPie.TabIndex = 2
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCerrar.BackColor = System.Drawing.Color.IndianRed
+        Me.btnCerrar.FlatAppearance.BorderSize = 0
+        Me.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCerrar.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCerrar.ForeColor = System.Drawing.Color.White
+        Me.btnCerrar.Location = New System.Drawing.Point(664, 13)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(110, 35)
+        Me.btnCerrar.TabIndex = 0
+        Me.btnCerrar.Text = "Cerrar"
+        Me.btnCerrar.UseVisualStyleBackColor = False
+        '
+        'frmDetalleDocumento
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.pnlCuerpo)
+        Me.Controls.Add(Me.pnlPie)
+        Me.Controls.Add(Me.pnlCabecera)
+        Me.MinimumSize = New System.Drawing.Size(600, 400)
+        Me.Name = "frmDetalleDocumento"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Detalle del Documento"
+        Me.pnlCabecera.ResumeLayout(False)
+        Me.gbDetalles.ResumeLayout(False)
+        Me.gbDetalles.PerformLayout()
+        Me.pnlCuerpo.ResumeLayout(False)
+        CType(Me.dgvMovimientos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlPie.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlCabecera As Panel
+    Friend WithEvents gbDetalles As GroupBox
+    Friend WithEvents lblNumero As Label
+    Friend WithEvents lblAsunto As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblEstado As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblUbicacion As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblFecha As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents pnlCuerpo As Panel
+    Friend WithEvents dgvMovimientos As DataGridView
+    Friend WithEvents pnlPie As Panel
+    Friend WithEvents btnCerrar As Button
+End Class
---
+++ VECTOR/frmDetalleDocumento.vb
@@ -0,0 +1,97 @@
+﻿Imports System.Data.Entity
+Imports System.Threading.Tasks
+
+Public Class frmDetalleDocumento
+
+    Private _idDocumento As Long
+
+    ' Constructor que recibe el ID
+    Public Sub New(idDoc As Long)
+        InitializeComponent()
+        _idDocumento = idDoc
+    End Sub
+
+    Private Async Sub frmDetalleDocumento_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            ' Aplicamos tu tema visual si existe
+            AppTheme.Aplicar(Me)
+
+            Me.Text = "Cargando detalles..."
+            Await CargarDatosAsync()
+        Catch ex As Exception
+            MessageBox.Show("Error al cargar detalle: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Async Function CargarDatosAsync() As Task
+        Using uow As New UnitOfWork()
+            Dim repo = uow.Repository(Of Mae_Documento)()
+
+            ' 1. Consultamos el documento con sus relaciones clave
+            ' Usamos Include para traer Tipo, Oficina y Movimientos en una sola consulta
+            Dim doc = Await repo.GetQueryable(tracking:=False) _
+                                .Include("Cat_TipoDocumento") _
+                                .Include("Cat_Oficina") _
+                                .Include("Cat_Estado") _
+                                .Include("Tra_Movimiento") _
+                                .Include("Tra_Movimiento.Cat_Oficina") _
+                                .Include("Tra_Movimiento.Cat_Usuario") _
+                                .FirstOrDefaultAsync(Function(d) d.IdDocumento = _idDocumento)
+
+            If doc Is Nothing Then
+                MessageBox.Show("El documento no existe o fue eliminado.")
+                Me.Close()
+                Return
+            End If
+
+            ' 2. Llenar Cabecera (Asegúrate de tener estos Labels en tu diseño)
+            Me.Text = "Detalle: " & doc.NumeroOficial
+
+            ' Supongamos que tienes labels llamados lblNumero, lblAsunto, lblEstado, etc.
+            ' Si no los tienes, puedes ajustar esto a tu diseño o usar un MsgBox de prueba.
+            lblNumero.Text = doc.Cat_TipoDocumento.Nombre & " " & doc.NumeroOficial
+            lblAsunto.Text = doc.Asunto
+            lblEstado.Text = doc.Cat_Estado.Nombre
+            lblUbicacion.Text = "Ubicación Actual: " & doc.Cat_Oficina.Nombre
+            lblFecha.Text = "Creado el: " & doc.FechaCreacion.Value.ToShortDateString()
+
+            ' Colorear estado según sea necesario
+            If doc.IdEstadoActual = 5 Then ' Anulado
+                lblEstado.ForeColor = Color.Red
+            Else
+                lblEstado.ForeColor = Color.DarkGreen
+            End If
+
+            ' 3. Llenar Grilla de Movimientos (Historial)
+            ' Proyectamos los datos para mostrar solo lo útil
+            Dim historial = doc.Tra_Movimiento.OrderByDescending(Function(m) m.FechaMovimiento).Select(Function(m) New With {
+                .Fecha = m.FechaMovimiento,
+                .Origen = If(m.Cat_Oficina IsNot Nothing, m.Cat_Oficina.Nombre, "N/A"),
+                .Destino = If(m.Cat_Oficina1 IsNot Nothing, m.Cat_Oficina1.Nombre, "N/A"),
+                .Responsable = If(m.Cat_Usuario IsNot Nothing, m.Cat_Usuario.UsuarioLogin, "Sistema"),
+                .Observacion = m.ObservacionPase
+            }).ToList()
+
+            dgvMovimientos.DataSource = historial
+            ConfigurarGrilla()
+
+        End Using
+    End Function
+
+    Private Sub ConfigurarGrilla()
+        With dgvMovimientos
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            .RowHeadersVisible = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .ReadOnly = True
+
+            ' Ajustes estéticos
+            If .Columns("Fecha") IsNot Nothing Then .Columns("Fecha").Width = 120
+            If .Columns("Observacion") IsNot Nothing Then .Columns("Observacion").FillWeight = 200
+        End With
+    End Sub
+
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Me.Close()
+    End Sub
+End Class
